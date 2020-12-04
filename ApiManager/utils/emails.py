91e55a97bf3f388945a2253a84fca76140fc1445
HANZIGUO <hanziguo@kingsoft.com>
2020-12-04 16:59:10 +0800

    email

--- ApiManager/utils/emails.py
+++ ApiManager/utils/emails.py
@@ -14,13 +14,11 @@ def send_email_reports(receiver, html_report_path):
     elif '@163.com' in EMAIL_SEND_USERNAME:
         smtp_server = 'smtp.163.com'
     else:
-        smtp_server = 'smtp.exmail.qq.com'
-
+        smtp_server = 'smtp.qq.com'
     subject = "接口自动化测试报告"
 
     with io.open(html_report_path, 'r', encoding='utf-8') as stream:
         send_file = stream.read()
-
     att = MIMEText(send_file, "base64", "utf-8")
     att["Content-Type"] = "application/octet-stream"
     att["Content-Disposition"] = "attachment;filename = TestReports.html"
@@ -33,14 +31,13 @@ def send_email_reports(receiver, html_report_path):
     msg['to'] = receiver
     msg.attach(att)
     msg.attach(body)
-
-    smtp = smtplib.SMTP()
-    smtp.connect(smtp_server)
-    smtp.starttls()
+    # smtp = smtplib.SMTP()
+    smtp = smtplib.SMTP_SSL(smtp_server, smtplib.SMTP_SSL_PORT)  # 如果端口是用SSL加密，请这样写代码。其中smtp是变量名
     smtp.login(EMAIL_SEND_USERNAME, EMAIL_SEND_PASSWORD)
     smtp.sendmail(EMAIL_SEND_USERNAME, receiver.split(','), msg.as_string())
     smtp.quit()
 
 
 if __name__ == '__main__':
-    send_email_reports('##@qq.com, example@163.com', 'D:\\HttpRunnerManager\\reports\\2018-06-05 15-58-00.html')
+    # send_email_reports('##@qq.com, example@163.com', 'D:\\HttpRunnerManager\\reports\\2018-06-05 15-58-00.html')
+    send_email_reports('han_ziguo@163.com', 'D:\\HttpRunnerManager\\reports\\2018-06-05 15-58-00.html')
